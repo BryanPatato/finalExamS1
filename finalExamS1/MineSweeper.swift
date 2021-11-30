b6b83abf80521f494528345ac51f445865614625
CarlS <carl.show@yahoo.com>
2021-11-30 08:09:43 -0600

    Minesweeper class update

--- finalExamS1/MineSweeper.swift
+++ finalExamS1/MineSweeper.swift
@@ -1,13 +1,32 @@
 import Foundation
+enum activeState
+{
+    case unplayed
+    case active
+    case clear
+    case explosion
+}
+enum tileType
+{
+    case flag
+    case mine
+    case empty
+    case number
+}
 public class mineSweeper
 {
-    
+    var state: activeState
+    var board: [[tileType]]
+    init()
+    {
+        state = .unplayed
+        
+    }
 }
-private class mine
+private class generator
 {
-    
-}
-private class flag
-{
-    
+    init(board: [[tileType]])
+    {
+        
+    }
 }
