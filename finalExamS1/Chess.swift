2749d944b8e5c841a14f50b0dd99d233819d246e
bryan <bryan.ruiz23@d155.org>
2021-11-30 07:51:02 -0600

    added a comment

--- finalExamS1/Chess.swift
+++ finalExamS1/Chess.swift
@@ -7,3 +7,11 @@
 
 import Foundation
 
+public class chess {
+    
+    
+}
+
+public class board {
+    
+}
--- finalExamS1/ViewController.swift
+++ finalExamS1/ViewController.swift
@@ -5,6 +5,8 @@
 //  Created by BRYAN RUIZ on 11/29/21.
 //  This is Carl
 
+//
+// this is bryan
 
 import UIKit
 
