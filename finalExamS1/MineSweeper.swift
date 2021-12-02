d3cb80a7c343f6f3e01c5bb8b72bb30fe7706944
CarlS <carl.show@yahoo.com>
2021-12-02 07:31:07 -0600

    Major updates to the minesweeper class

--- finalExamS1/MineSweeper.swift
+++ finalExamS1/MineSweeper.swift
@@ -20,29 +20,52 @@ public class mineSweeper
     init()
     {
         state = .unplayed
-        board = generator(board: board).generate()
+        let c = generator(boardSizeX: 8, BoardSizeY: 8)
+        board = c.generate()
     }
 }
 private class generator
 {
-    var bombHandler = [0, 0.0, 0.0]
-    var activeBoard: [[tileType]]
-    init(board: [[tileType]])
+    var bombHandler = [0, 0.0, 0]
+    var x: Int
+    var y: Int
+    init(boardSizeX: Int, BoardSizeY: Int)
     {
-        activeBoard = board
+        x = boardSizeX
+        y = BoardSizeY
+        let base = boardSizeX * BoardSizeY
         // Base case is an 8 by 8 board
         // in the base case, this would make 20 of the 64 tiles bombs
-        bombHandler[0] = floor(Double(Double(board.count)/3.17))
-        // in the base case, this would make the bomb density .875 ( This handles extra bombs on larger boards, thus the new bomb total is 22 bombs )
-        bombHandler[1] = (56/Double(board.count))
-        // in the base case, this would randomly assign a bomb tenacity between 1 and 4    ( This handles bomb clumping )
-        bombHandler[2] = CGFloat.random(in: 0...floor(CGFloat(board.count/16 - 1))) + 1
+        bombHandler[0] = floor(Double(Double(base)/3.17))
+        // in the base case, this would make the bomb density 1 ( This handles extra bombs on larger boards, thus the new bomb total is 20 bombs )
+        bombHandler[1] = (64/Double(base))
+        // in the base case, this would randomly assign a bomb tenacity between 1 and 3    ( This handles bomb clumping )
+        bombHandler[2] = CGFloat.random(in: 0...floor(CGFloat(base/16)) - 1)
         bombHandler[0] /= floor(bombHandler[1])
-        // The reason behind this one here is to make smaller boards have less clumping, yet larger ones happen to clump more-- heck, this actually disincetivises active clumping in the base case, making the lowest tendancy value a -2.
+        // The reason behind this one here is to make smaller boards have less clumping, yet larger ones happen to clump more-- heck, this actually disincetivises active clumping in the base case, making the maximum tendancy value a 1.
         bombHandler[2] -= ceil(bombHandler[1]*2)
     }
     func generate() -> [[tileType]]
     {
-        
+        var newBoard = [[tileType]](repeating: [tileType](repeating: .empty, count: y), count: x)
+        for _ in 0...Int(bombHandler[0])
+        {
+            var randPlaceX = CGFloat.random(in: 0...CGFloat(x))
+            var randPlaceY = CGFloat.random(in: 0...CGFloat(y))
+            for _ in 0...Int(bombHandler[2])
+            {
+                if  newBoard[Int(randPlaceX) - 1][Int(randPlaceY)] == .mine || newBoard[Int(randPlaceX) + 1][Int(randPlaceY)] == .mine || newBoard[Int(randPlaceX)][Int(randPlaceY) - 1] == .mine || newBoard[Int(randPlaceX)][Int(randPlaceY) + 1] == .mine
+                {
+                    break
+                }
+                else
+                {
+                    randPlaceX = CGFloat.random(in: 0...CGFloat(x))
+                    randPlaceY = CGFloat.random(in: 0...CGFloat(y))
+                }
+            }
+            newBoard[Int(randPlaceX)][Int(randPlaceY)] = .mine
+        }
+        return newBoard
     }
 }
