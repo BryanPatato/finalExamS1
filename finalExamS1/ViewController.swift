4d02a54f6844e4992efcaa9e012424157c23639e
CarlS <carl.show@yahoo.com>
2021-12-02 07:53:07 -0600

    Maybe this'll work?

--- finalExamS1/ViewController.swift
+++ finalExamS1/ViewController.swift
@@ -9,13 +9,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
- var int = 0
-    override func viewDidLoad() {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource
+{
+    @IBOutlet weak var use: UISegmentedControl!
+    @IBOutlet weak var cellCell: UICollectionView!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return 64
+    }
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell = cellCell.dequeueReusableCell(withReuseIdentifier: "selCel", for: indexPath)
+        cell.backgroundColor = #colorLiteral(red: 0, green: 0.6180339456, blue: 1, alpha: 0.8438409567)
+        return cell
+    }
+    func collectionView(_ colectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
+    {
+        let cell = collectionView(colectionView, cellForItemAt: indexPath)
+        let preColor = cell.backgroundColor
+        if use.selectedSegmentIndex == 0
+        {
+            cell.backgroundColor = UIColor.black
+        }
+        else
+        {
+            cell.backgroundColor = UIColor.gray
+        }
+        UICollectionViewCell.animate(withDuration: 2, delay: 0.2, options: .curveEaseInOut, animations:
+            {cell.backgroundColor = preColor;})
+    }
 }
 
